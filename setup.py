4e9bd4610c239b3502208e7d552a4d72e2a2dd29
minillinim <mike@mikeimelfort.com>
2014-08-20 16:17:15 +1000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DancingPeasant',
-    version='0.1.5',
+    version='0.1.6',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['dancingPeasant', 'dancingPeasantTest'],
